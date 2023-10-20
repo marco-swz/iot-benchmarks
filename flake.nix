714e37c6806add01d81f779db130cee60921b9be
xRetry <marcoschwarz.at@gmail.com>
2023-10-20 16:15:04 +0200

    Add ros2 to flake

--- flake.nix
+++ flake.nix
@@ -1,29 +1,34 @@
 {
-  description = "A very basic flake";
+    description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-  };
-
-  outputs = inputs@{ self, nixpkgs, flake-utils, rust-overlay, ... }:
-  flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
-	pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlay
-            ];
-          };
-  in rec {
-    devShell = pkgs.mkShell {
-      buildInputs = with pkgs; [ 
-	cmake
-	rust-analyzer
-	rust-bin.stable.latest.default
-	pkg-config
-	openssl.dev
-      ];
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+        rust-overlay.url = "github:oxalica/rust-overlay";
+        ros-overlay.url = "github:lopsided98/nix-ros-overlay";
     };
-  });
+
+    outputs = inputs@{ self, nixpkgs, flake-utils, rust-overlay, ros-overlay, ... }:
+        flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: 
+            let
+                pkgs = import nixpkgs {
+                    inherit system;
+                    overlays = [
+                        rust-overlay.overlays.default
+                        ros-overlay.overlays.default
+                    ];
+                };
+            in rec {
+                devShell = pkgs.mkShell {
+                    buildInputs = with pkgs; [ 
+                        cmake
+                        rust-analyzer
+                        rust-bin.stable.latest.default
+                        pkg-config
+                        openssl.dev
+                        rosPackages.humble.ros-core
+                        colcon
+                    ];
+                };
+            });
 }
