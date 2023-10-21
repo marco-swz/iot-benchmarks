daf230946c151795cc609eab71d54d1daf9a6e85
xRetry <marcoschwarz.at@gmail.com>
2023-10-21 13:53:54 +0200

    Fix bindgen in nix flake

--- flake.nix
+++ flake.nix
@@ -18,6 +18,8 @@
                         ros-overlay.overlays.default
                     ];
                 };
+                stdenv = pkgs.stdenv;
+                lib = pkgs.lib;
             in rec {
                 devShell = pkgs.mkShell {
                     buildInputs = with pkgs; [ 
@@ -28,7 +30,25 @@
                         openssl.dev
                         rosPackages.humble.ros-core
                         colcon
+                        #llvmPackages_16.clang-unwrapped
                     ];
+
+                    LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
+
+                    shellHook = ''
+                        # From: https://github.com/NixOS/nixpkgs/blob/1fab95f5190d087e66a3502481e34e15d62090aa/pkgs/applications/networking/browsers/firefox/common.nix#L247-L253
+                        # Set C flags for Rust's bindgen program. Unlike ordinary C
+                        # compilation, bindgen does not invoke $CC directly. Instead it
+                        # uses LLVM's libclang. To make sure all necessary flags are
+                        # included we need to look in a few places.
+                        export BINDGEN_EXTRA_CLANG_ARGS="$(< ${stdenv.cc}/nix-support/libc-crt1-cflags) \
+                          $(< ${stdenv.cc}/nix-support/libc-cflags) \
+                          $(< ${stdenv.cc}/nix-support/cc-cflags) \
+                          $(< ${stdenv.cc}/nix-support/libcxx-cxxflags) \
+                          ${lib.optionalString stdenv.cc.isClang "-idirafter ${stdenv.cc.cc}/lib/clang/${lib.getVersion stdenv.cc.cc}/include"} \
+                          ${lib.optionalString stdenv.cc.isGNU "-isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc} -isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${lib.getVersion stdenv.cc.cc}/include"} \
+                        "
+                      '';
                 };
             });
 }
